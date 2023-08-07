223e21f827ee8e6cd923b396658fce049c9d7faa
Hayden Caffey <hayden@Haydens-MacBook-Pro-2.local>
2023-08-06 23:19:38 -0400

    final still broken mongo

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const { connect, connection } = require('mongoose');
 
 // there is some issue here that continues to give me ECONNREFUSED errors, may be compounded w dependency version issue?
-connect('mongodb://127.0.0.1/', {
+connect('mongodb://127.0.0.1/developersApplications', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
